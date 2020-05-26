44a2d38d1fa4be978ec439c9439c69e944726375
Robert Ing <robertming@gmail.com>
2020-05-26 13:18:24 -0400

    Update event handler

--- src/event-handler.js
+++ src/event-handler.js
@@ -1,7 +1,3 @@
-var GETOFFER = 'getOffer';
-// for future v2 updates for SPAs
-var TRIGGERVIEW = 'TRIGGERVIEW';
-
 function EventHandler(common) {
     this.common = common || {};
 }
@@ -10,8 +6,8 @@ EventHandler.prototype.logEvent = function(event) {
     var MBOXNAME = event.CustomFlags['ADOBETARGET.MBOX'];
     var successHandler = event.CustomFlags['ADOBETARGET.SUCCESS'];
     var errorHandler = event.CustomFlags['ADOBETARGET.ERROR'];
-    var getOffer = Boolean(event.CustomFlags['ADOBETARGET.GETOFFER']);
-    var timeout = Boolean(event.CustomFlags['ADOBETARGET.TIMEOUT']);
+    var getOffer = event.CustomFlags['ADOBETARGET.GETOFFER'];
+    var timeout = event.CustomFlags['ADOBETARGET.TIMEOUT'];
 
     if (!MBOXNAME) {
         console.log(
@@ -19,36 +15,36 @@ EventHandler.prototype.logEvent = function(event) {
         );
         return false;
     }
-    var options = {
-        mbox: MBOXNAME,
-        params: params,
-        success: function(offer) {
-            window.adobe.target.applyOffer(offer);
-            if (event.CustomFlags['ADOBE.SUCCESS']) {
-                successHandler(offer);
-            }
-        },
-        error: function(status, error) {
-            errorHandler(status, error);
-        },
-    };
 
     var params = {};
     for (var key in event.EventAttributes) {
         params[key] = event.EventAttributes[key];
     }
 
+    var options = {
+        mbox: MBOXNAME,
+        params: params,
+    };
+
     // an event is either a getOffer event or a trackEvent event
     if (getOffer) {
-        window.adobe.target.getOffer({
-            timeout: timeout,
-        });
+        options.success = function(offer) {
+            window.adobe.target.applyOffer(offer);
+            if (successHandler) {
+                successHandler(offer);
+            }
+        };
+        options.error = function(status, error) {
+            if (errorHandler) {
+                errorHandler(status, error);
+            }
+        };
+        window.adobe.target.getOffer(options);
     } else {
-        var selector = Boolean(event.CustomFlags['ADOBETARGET.SELETOR']);
-        var type = Boolean(event.CustomFlags['ADOBETARGET.TYPE']);
-        var preventDefault = Boolean(
-            event.CustomFlags['ADOBETARGET.PREVENTDEFAULT']
-        );
+        var selector = event.CustomFlags['ADOBETARGET.SELECTOR'];
+        var type = event.CustomFlags['ADOBETARGET.TYPE'];
+        var preventDefault = event.CustomFlags['ADOBETARGET.PREVENTDEFAULT'];
+
         if (selector) {
             options.selector = selector;
         }
@@ -61,10 +57,8 @@ EventHandler.prototype.logEvent = function(event) {
         if (timeout) {
             options.timeout = timeout;
         }
-        window.adobe.target.trackEvent({
-            mbox: MBOXNAME,
-            params: params,
-        });
+
+        window.adobe.target.trackEvent(options);
     }
 
     return true;
@@ -73,23 +67,8 @@ EventHandler.prototype.logEvent = function(event) {
 EventHandler.prototype.logError = function() {};
 
 EventHandler.prototype.logPageView = function(event) {
-    var MBOXNAME = event.CustomFlags['ADOBE.MBOX'];
-    if (!MBOXNAME) {
-        console.log(
-            'ADOBE.MBOX not passed as custom flag; not forwarding to Adobe Target'
-        );
-        return false;
-    }
-
-    var params = {};
-    for (var key in event.EventAttributes) {
-        params[key] = event.EventAttributes[key];
-    }
-
-    window.adobe.target.trackEvent({
-        mbox: MBOXNAME,
-        params: params,
-    });
+    debugger;
+    this.logEvent(event);
 };
 
 module.exports = EventHandler;
